88972e205ab06d09b448f18a63648a2a8db465c0
Jojola07 <jojola10@proton.me>
2026-06-24 17:59:07 -0300

    script de leaderstats

--- src/server/PicaretaScript.lua
+++ src/server/PicaretaScript.lua
@@ -1,21 +1,12 @@
-local workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 
-local pedra = workspace:WaitForChild("Pedra")
-
-local hits = 0
-
 Players.PlayerAdded:Connect(function(jogador)
-    jogador.CharacterAdded:Connect(function(personagem)
-        local picareta = personagem:WaitForChild("Picareta")
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = jogador
 
-        picareta.Activated:Connect(function()
-            hits = hits + 1
-            print("Hit " .. hits)
-
-            if hits >= 2 then
-                pedra:Destroy()
-            end
-        end)
-    end)
+    local moedas = Instance.new("IntValue")
+    moedas.Name = "Moedas"
+    moedas.Value = 0
+    moedas.Parent = leaderstats
 end)
