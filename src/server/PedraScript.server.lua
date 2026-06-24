9e92d245a59b8801f0db4e0dcc63d1338ebaa290
Jojola07 <jojola10@proton.me>
2026-06-24 16:39:24 -0300

    verificação se tem picareta pra bater

--- src/server/PedraScript.server.lua
+++ src/server/PedraScript.server.lua
@@ -3,5 +3,12 @@ local workspace = game:GetService("Workspace")
 local pedra = workspace:WaitForChild("Pedra")
 
 pedra.ClickDetector.MouseClick:Connect(function(jogador)
-    print(jogador.Name .. " bateu na pedra")
+    local personagem = jogador.Character
+    local picareta = personagem:FindFirstChild("Picareta")
+
+    if picareta then
+    print(jogador.Name .. " bateu na pedra com a picareta")
+    else
+        print(jogador.Name .. " não tem picareta equipada")
+    end
 end)
